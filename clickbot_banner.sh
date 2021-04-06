6f7459943917b0ad32c3e7a2ce9c870939a9fed6
Alkaid-10 <53359190+Alkaid-10@users.noreply.github.com>
2021-04-06 12:06:23 +0800

    Create clickbot_banner.sh

---
+++ clickbot_banner.sh
@@ -0,0 +1,8 @@
+adb shell am start  com.ifmvo.togetherad.demo/.MainActivity
+sleep 3
+adb shell am start com.ifmvo.togetherad.demo/.banner.BannerActivity
+sleep 3
+adb shell input tap 583 1700
+sleep 1
+adb shell am start  com.ifmvo.togetherad.demo/.MainActivity
+
