6357facb3a6f997ff1a630dd1facbb07dc82370a
Alkaid-10 <53359190+Alkaid-10@users.noreply.github.com>
2021-04-06 12:05:53 +0800

    Create clickbot_start.sh

---
+++ clickbot_start.sh
@@ -0,0 +1,7 @@
+adb shell am start  com.ifmvo.togetherad.demo/.MainActivity
+sleep 3
+adb shell am start com.ifmvo.togetherad.demo/.splash.SplashHotActivity
+sleep 4
+adb shell input tap 546 1787
+sleep 5
+adb shell am start  com.ifmvo.togetherad.demo/.MainActivity
